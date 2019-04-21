0e6097f530d79806b2db4585a7cc32487e8949ab
AbdSab <abdellah.sabiri.1995@gmail.com>
2019-04-21 13:59:28 +0100

    Setting up base

---
+++ src/bot.ts
@@ -0,0 +1,14 @@
+import Discord from 'discord.js';
+import config from '../config.json';
+
+export class Bot{
+    token: string;
+    
+    public constructor(token: string) {
+        this.token = token;
+    }
+
+    public start(){
+        
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,9 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-
+import {Bot} from './bot';
 
 
 const connection = createConnection()
 .then(connection => {
     console.log("Tables created !");
 }).catch(error => console.log(error));
-/*
-createConnection({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "db_team_maker",
-    entities: [
-        User, Team, Type, Guild, Category
-    ],
-    synchronize: true,
-    logging: false
-}).then(connection => {
-    console.log("Tables created !");
-}).catch(error => console.log(error));*/
